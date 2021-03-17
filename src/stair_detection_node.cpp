e5cf7ec7d68af7589da928c73134fd7077245d9c
He Zhang <hxzhang1@ualr.edu>
2021-03-17 00:26:14 -0400

    test edge based method

---
+++ src/stair_detection_node.cpp
@@ -0,0 +1,195 @@
+/*
+    Mar. 16, 2021, He Zhang, fuyinzh@gmail.com 
+    
+    receive camera data to detect the staircase 
+
+*/
+
+
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/Imu.h>
+#include <std_msgs/Bool.h>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <execinfo.h>
+#include <csignal>
+
+#include <opencv2/opencv.hpp>
+#include <eigen3/Eigen/Dense>
+#include "stair_detector_geo.h"
+
+using namespace std; 
+
+StairDetectorGeo sdg;
+StairDetectorGeoParams param;
+
+queue<sensor_msgs::ImageConstPtr> img_buf;
+double first_image_time;
+int pub_count = 1;
+bool first_image_flag = true;
+double last_image_time = 0;
+bool init_pub = 0;
+
+string IMAGE_TOPIC = "/cam0/color";
+string DEPTH_TOPIC = "/cam0/depth"; 
+
+void img_callback(const sensor_msgs::ImageConstPtr &color_msg, const sensor_msgs::ImageConstPtr &depth_msg)
+{
+    if(first_image_flag)
+    {
+        first_image_flag = false;
+        first_image_time = color_msg->header.stamp.toSec();
+        last_image_time = color_msg->header.stamp.toSec();
+        return;
+    }
+    // detect unstable camera stream
+    if (color_msg->header.stamp.toSec() - last_image_time > 1.0 || color_msg->header.stamp.toSec() < last_image_time)
+    {
+        ROS_WARN("image discontinue! reset the feature tracker!");
+        first_image_flag = true;
+        last_image_time = 0;
+        pub_count = 1;
+        return;
+    }
+    last_image_time = color_msg->header.stamp.toSec();
+
+    // encodings in ros: http://docs.ros.org/diamondback/api/sensor_msgs/html/image__encodings_8cpp_source.html
+    //color has encoding RGB8
+    cv_bridge::CvImageConstPtr ptr;
+    cv::Mat ret_img; 
+    if (color_msg->encoding == "8UC1")//shan:why 8UC1 need this operation? Find answer:https://github.com/ros-perception/vision_opencv/issues/175
+    {
+        sensor_msgs::Image img;
+        img.header = color_msg->header;
+        img.height = color_msg->height;
+        img.width = color_msg->width;
+        img.is_bigendian = color_msg->is_bigendian;
+        img.step = color_msg->step;
+        img.data = color_msg->data;
+        img.encoding = "mono8";
+        ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8);
+        ret_img = ptr->image; 
+    } else if(color_msg->encoding == "8UC3"){
+        sensor_msgs::Image img;
+        img.header = color_msg->header;
+        img.height = color_msg->height;
+        img.width = color_msg->width;
+        img.is_bigendian = color_msg->is_bigendian;
+        img.step = color_msg->step;
+        img.data = color_msg->data;
+        img.encoding = "bgr8";
+        ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+        ret_img = ptr->image.clone(); 
+        cv::cvtColor(ret_img, ret_img, cv::COLOR_BGR2GRAY);
+    }
+    else{
+        ptr = cv_bridge::toCvCopy(color_msg, sensor_msgs::image_encodings::MONO8);
+        ret_img = ptr->image.clone(); 
+    }
+
+    //depth has encoding TYPE_16UC1
+    cv_bridge::CvImageConstPtr depth_ptr;
+    // debug use     std::cout<<depth_msg->encoding<<std::endl;
+    if (depth_msg->encoding == "16UC1")
+    {
+        sensor_msgs::Image img;
+        img.header = depth_msg->header;
+        img.height = depth_msg->height;
+        img.width = depth_msg->width;
+        img.is_bigendian = depth_msg->is_bigendian;
+        img.step = depth_msg->step;
+        img.data = depth_msg->data;
+        img.encoding = "mono16"; // sensor_msgs::image_encodings::MONO16;
+        depth_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO16);
+    }else
+        depth_ptr = cv_bridge::toCvCopy(depth_msg, sensor_msgs::image_encodings::MONO16);
+
+
+    cv::Mat show_img = ret_img; //ptr->image;
+    cv::Mat dep_img = depth_ptr->image.clone(); 
+
+
+   // cv::imshow("vis", show_img);
+   // cv::waitKey(5);
+   cv::imshow("dep", dep_img); 
+   cv::waitKey(5); 
+
+
+    std::vector<cv::Point> show_bounding_box;
+    try
+    {
+        // cv::Mat depth_image = cv_bridge::toCvCopy(msg, "8UC1")->image;
+        cv::Mat depth_image = dep_img;
+        // dep_img.convertTo(depth_image, CV_8UC1, 255./5000.); // maximum 5 meters 
+
+        cv::imshow("Depth_image", depth_image); 
+        cv::waitKey(10);
+        if (sdg.getStairs(depth_image, show_bounding_box)) {
+          
+            std::cout << "Found Potential Stiars" << std::endl;
+            sdg.drawBox(show_img, show_bounding_box);
+        
+            cv::imshow("Result", show_img);
+            cv::waitKey(10);
+
+        } else {
+         
+            std::cout << "Can't find stiars" << std::endl;
+        }
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        // ROS_ERROR("Could not convert from '%s' to '8UC1'.", msg->encoding.c_str());
+    }
+
+
+    // init pts here, using readImage()
+    ROS_INFO("depth_image size: width: %d height: %d", dep_img.cols, dep_img.rows);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "stair_detection_node");
+    ros::NodeHandle n("~");
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+
+    cv::namedWindow("Result");
+    cv::namedWindow("Depth_image");
+    cv::startWindowThread();
+
+    // param.debug = false;
+    param.fill_invalid = false;
+    param.ignore_invalid = false;
+    param.use_laplacian = false;
+    param.debug = true; 
+    sdg.setParam(param);
+
+    //ref: http://docs.ros.org/api/message_filters/html/c++/classmessage__filters_1_1TimeSynchronizer.html#a9e58750270e40a2314dd91632a9570a6
+    //     https://blog.csdn.net/zyh821351004/article/details/47758433
+    message_filters::Subscriber<sensor_msgs::Image> sub_image(n, IMAGE_TOPIC, 1);
+    message_filters::Subscriber<sensor_msgs::Image> sub_depth(n, DEPTH_TOPIC, 1);
+//    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync(sub_image, sub_depth, 100);
+    // use ApproximateTime to fit fisheye camera
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,sensor_msgs::Image> syncPolicy;
+    message_filters::Synchronizer<syncPolicy> sync(syncPolicy(10), sub_image, sub_depth);
+    sync.registerCallback(boost::bind(&img_callback, _1, _2));
+
+    ros::spin();
+    cv::destroyWindow("Result");
+    cv::destroyWindow("Depth_image");
+    return 0;
+}
+
+
+// new points velocity is 0, pub or not?
+// track cnt > 1 pub?
---
+++ src/stair_detector_geo.h
@@ -0,0 +1,278 @@
+#pragma once
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "neighbour_finder.h"
+#define PI 3.141592654
+
+class Line {
+public:
+	// In opencv x is col, y is row
+	//
+	// @param[in]  x1    The x1
+	// @param[in]  y1    The y1
+	// @param[in]  x2    The x2
+	// @param[in]  y2    The y2
+	//
+	Line(double x1 = 0, double y1 = 0, double x2 = 0, double y2 = 0) {
+		this->k = (y1 - y2) / (x1 - x2);
+		this->b = y1 - k * x1;
+		// this->t = - atan2( y1 - y2, x1 - x2);
+		this->t = atan((y1 - y2) / (x1 - x2));
+		this->r = x1 * sin(t) + y1 * cos(t);
+		this->p1 = *new cv::Point(x1, y1);
+		this->p2 = *new cv::Point(x2, y2);
+		this->center = p1 - p2;
+		this->length = cv::norm(this->p1 - this->p2);
+	}
+
+	Line(cv::Point p1, cv::Point p2) : Line(p1.x, p1.y, p2.x, p2.y) {}
+
+	Line(cv::Vec4i line) : Line(line[0], line[1], line[2], line[3]) {}
+
+	Line() {}
+
+	/**
+	 * @brief      Calculate Pixels of the line on the image
+	 *
+	 * @param[in]  src   The source image
+	 */
+	void calPixels(cv::Mat src) {
+		this->pixels.clear();
+		cv::LineIterator it(src, p1, p2, 8, true);
+		pixels_num = it.count;
+		for (int i = 0; i < pixels_num; i++, ++it) {
+			pixels.push_back(it.pos());
+		}
+	}
+
+	friend std::ostream &operator<<(std::ostream &os, const Line &line) {
+		os << "Point1: " << line.p1 << " Point2: " << line.p2 << " k:" << line.k
+		   << " b:" << line.b << " t: " << line.t << " r: " << line.r;
+		return os;
+	}
+
+	double k, b = 0;
+	double r, t = 0;
+	double length = 0;
+	cv::Point p1;
+	cv::Point p2;
+	cv::Point center;
+	int pixels_num = 0;
+	// cv::LineIterator it;
+	std::vector<cv::Point> pixels;
+};
+typedef std::vector<Line> Lines;
+
+struct StairDetectorGeoParams
+{
+	// high level bools
+	bool show_result = false;
+	bool debug = false;
+	bool ignore_invalid = false;
+	bool fill_invalid = false;
+	bool use_laplacian = false;
+	// canny
+	double canny_low_threshold = 40;
+	double canny_ratio = 10;
+	int    canny_kernel_size = 1;
+	// hough transfrom
+	double hough_min_line_length = 25; // pixel distance
+	double hough_max_line_gap = 15;   // pixel distance
+	double hough_threshold = 20;      //
+	int    hough_rho = 8;            //
+	double hough_theta = 1;           // angle
+	// filter by slope histogram
+	double filter_slope_hist_bin_width = 20;  // degree
+	// filter by hard slope
+	double filter_slope_bandwidth = 10; // angle
+	// merge parameters
+	double merge_max_dist_diff = 20; // pixel
+	double merge_max_angle_diff = 30; // degree
+	int    merge_close_count = 10; // number
+	// bounding box
+	int    minimum_line_num = 3;
+};
+
+
+class StairDetectorGeo {
+public:
+	StairDetectorGeo();
+
+	/**
+	 * @brief      Main function to get stairs
+	 *
+	 * @param[in]  input_image   The input image
+	 * @param      bounding_box  The bounding box
+	 *
+	 * @return     The stairs.
+	 */
+	bool getStairs(const cv::Mat& input_image, std::vector<cv::Point>& bounding_box);
+
+	/**
+	 * @brief      Sets the parameter.
+	 *
+	 * @param[in]  param  The parameter
+	 */
+	void setParam(const StairDetectorGeoParams& param);
+
+	/**
+	* @brief      Draws xy lines.
+	*
+	* @param      image  The image
+	* @param[in]  lines  The lines
+	*
+	*/
+
+	void drawLines(cv::Mat &image, const Lines &lines, const cv::Scalar &color);
+	/**
+	 * @brief      Draws line's slope.
+	 *
+	 * @param      image  The image
+	 * @param[in]  lines  The lines
+	 * @param[in]  color  The color
+	 */
+	void drawLinesSlope(cv::Mat &image, const Lines &lines, const cv::Scalar &color);
+
+	/**
+	 * @brief      Draws line's radius.
+	 *
+	 * @param      image  The image
+	 * @param[in]  lines  The lines
+	 * @param[in]  color  The color
+	 */
+	void drawLinesRadius(cv::Mat &image, const Lines &lines, const cv::Scalar &color);
+
+	/**
+	 * @brief      Draws a box.
+	 *
+	 * @param[in]  image         The image
+	 * @param[in]  bounding_box  The bounding box
+	 */
+	void drawBox(cv::Mat& image, const std::vector<cv::Point>& bounding_box);
+
+
+	/**
+	 * @brief      Visualize the histogram
+	 *
+	 * @param[in]  hist         The hist
+	 * @param[in]  height       The height
+	 * @param[in]  window_name  The window name
+	 */
+	void visualizeHist(const std::vector<int> hist, int height, std::string window_name);
+
+
+	/**
+	 * @brief      calculate the hough lines
+	 *
+	 * @param[in]  edge_image  The edge image
+	 * @param      lines       The lines
+	 */
+	void houghLine(const cv::Mat &edge_image, Lines &lines);
+
+	/**
+	 * @brief      filter lines by a specific threshold and buff size
+	 *
+	 * @param[in]  input_lines     The input lines
+	 * @param      filtered_lines  The filtered lines
+	 */
+	void filterLinesBySlopeThreshold(const Lines& input_lines, Lines& filtered_lines );
+
+	/**
+	 * @brief      filter lines by slope histogram
+	 *
+	 * @param[in]  input_lines     The input lines
+	 * @param      filtered_lines  The filtered lines
+	 */
+	void filterLinesBySlopeHist(const Lines& input_lines, Lines& filtered_lines );
+
+	/**
+	 * @brief      Gets the bounding box.
+	 *
+	 * @param[in]  input_lines   The input lines
+	 * @param      bounding_box  The bounding box
+	 *
+	 * @return     The bounding box.
+	 */
+	bool getBoundingBox(const cv::Mat input_rgb_image, const Lines &input_lines, std::vector<cv::Point>& bounding_box);
+
+private:
+	// cv::Mat src_rgb_, src_gray_, edge_image_;
+	// cv::Mat debug_image1_, debug_image2_;
+	StairDetectorGeoParams param_;
+	/**
+	 * @brief      Gets the neighboor pixel identifier.
+	 *
+	 * @param[in]  row   The row
+	 * @param[in]  col   The col
+	 * @param      ids   The identifiers
+	 *
+	 * @return     if it's success or not
+	 */
+	bool getNeighbourId(int row, int col, std::vector<cv::Vec2i> &ids, const cv::Mat& input_image);
+
+
+	/**
+	 * @brief      Determines if lines are close.
+	 *
+	 * @param[in]  line1  The line 1
+	 * @param[in]  line2  The line 2
+	 *
+	 * @return     True if close, False otherwise.
+	 */
+	bool isLineClose(const Line &line1, const Line &line2);
+
+	/**
+	 * @brief      Determines if line parallel.
+	 *
+	 * @param[in]  line1  The line 1
+	 * @param[in]  line2  The line 2
+	 *
+	 * @return     True if line parallel, False otherwise.
+	 */
+	bool isLineParallel(const Line &line1, const Line &line2);
+
+	typedef std::vector<std::vector<cv::Point> > MergePointsList;
+	/**
+	 * @brief      Get the merge list form the input lines, every line that can be
+	 *             merged will be put in merge point list, any line that can't be
+	 *             merged will be put in pre_merge_lines
+	 *
+	 * @param[in]  input_lines        The input lines
+	 * @param      merge_points_list  The merge points list, type
+	 * std::vector<std::vector<cv::Point> >
+	 * @param      pre_merge_lines    The pre merge lines
+	 *
+	 * @return     void
+	 */
+	void getMergePointList(const Lines &input_lines, MergePointsList &merge_points_list, Lines &pre_merge_lines);
+
+	/**
+	 * @brief      merge all lines
+	 *
+	 * @param[in]  input_lines   The input XY lines
+	 * @param      merged_lines  The merged lines
+	 */
+	void mergeLines(const cv::Mat& input_image, const Lines &input_lines, Lines &merged_lines);
+
+	void ignoreInvalid(const cv::Mat& input_image, cv::Mat& filter_image);
+
+	/**
+	 * @function CannyThreshold
+	 * @brief      Trackbar callback - Canny thresholds input with a ratio_ 1:3
+	 *
+	 * @param[in]  src   The source image should be GraySclae
+	 * @param      edge  The out put edge image is Gray Scale
+	 */
+	void cannyEdgeDetection(const cv::Mat& input_image, cv::Mat &edge);
+	void sobelEdgeDetection(const cv::Mat& input_image, cv::Mat &edge);
+	void laplacianEdgeDetection(const cv::Mat& input_image, cv::Mat &edge);
+
+	void fillByNearestNeighbour(const cv::Mat &image, cv::Mat& filled_image);
+
+	bool isInbound(int x, int y, const cv::Mat& image);
+
+}; // end of class StairDetectorGeo
